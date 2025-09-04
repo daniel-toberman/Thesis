10ec9aacac146a07108bdb9030a3150be9bc570d
daniel toberman <daniel.toberman@nyxoah.com>
2025-09-04 16:13:46 +0300

    load noise bug fix?

--- SSL/RecordData.py
+++ SSL/RecordData.py
@@ -126,8 +126,9 @@ class RealData(Dataset):
 
     def load_noise(self, noise_path, begin_index, end_index, use_mic_id):
         channels = []
+        root, ext = os.path.splitext(noise_path)  # ext is ".wav"
         for i in use_mic_id:
-            temp_path = noise_path.replace('_CH1.flac', f'_CH{i}.wav')
+            temp_path = noise_path.replace(f'_CH1{ext}', f'_CH{i}{ext}')
             try:
                 single_ch_signal, fs = sf.read(
                     temp_path, start=begin_index, stop=end_index, dtype="float32"
